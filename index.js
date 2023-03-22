fa668b28f39cc303e30079f534b4b82c65f1551f
SifatNiloy <sifatniloy18@gmail.com>
2023-03-22 17:46:19 +0600

    pagination done

--- index.js
+++ index.js
@@ -17,9 +17,12 @@ async function run() {
         const productCollection = client.db("emaJohn").collection("products");
 
         app.get('/products', async (req, res) => {
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+            // console.log(page, size)
             const query = {};
             const cursor = productCollection.find(query)
-            const products = await cursor.toArray();
+            const products = await cursor.skip(page * size).limit(size).toArray();
             const count = await productCollection.estimatedDocumentCount();
             res.send( {count, products})
         })
